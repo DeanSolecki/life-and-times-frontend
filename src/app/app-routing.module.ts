68ea34348f702a0912719adc7da2c8d1c3fdbde5
Dean Solecki <deansolecki@gmail.com>
2017-11-25 17:38:56 -0800

    Add routing, scaffold blog, code, about components, add navbar and sidebar.  Minor css tweaks.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LandingComponent } from './landing/landing.component';
+import { BlogComponent } from './blog/blog.component';
+import { CodeComponent } from './code/code.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'code', component: CodeComponent },
+  { path: 'about', component: AboutComponent }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';
+import { BlogComponent } from './blog/blog.component';
+import { CodeComponent } from './code/code.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    AppSidebarComponent,
+    BlogComponent,
+    CodeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
